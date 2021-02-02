5c5b1b51ca7fdf1cdc7db4bfd11ce1e8a0212310
unknown <ahedqaood19@gmail.com>
2021-02-02 14:40:19 +0300

    fix an issue

--- server/server.js
+++ server/server.js
@@ -4,30 +4,10 @@ const fs = require('fs')
 const express = require('express');
 const app = express();
 const server  = require('http').createServer(app);
-const io      = require('socket.io').listen(server);
-// app.set('socketio', io);
 const cors = require('cors');
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cors());
-
-// io.on("connection", (socket) =>{
-//   console.log("Socket.io connection has started!");
-// });
-
-app.get('/order', async (req, res) => {
-  // socket connection handler
-  io.sockets.on('connection', function (client) {
-    // this works great:    
-    client.emit('text_msg', {msg: 'Welcome you are now connected.'});
-  });
-
-  // io.emit('order:userPlacedOrder', 'new_order');
-  // console.log('newOrder');
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Successful notification\n');
-});
 
 app.get('/test', async (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
